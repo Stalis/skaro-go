7dbe9ada2e221e45b8f90ae83f8568e307e3fdf5
Stanislaw Babkin <stalissss@outlook.com>
2017-05-01 23:44:54 +0300

    Add -host flag

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"skaro-im/skarolib-go"
 )
 
+var host = flag.String("host", "skaro.stalis-dev.xyz:8000", "http service address")
+
 func main() {
 	log.Println("Starting...")
-	skaro.StartConnection("skaro.stalis-dev.xyz:8000")
+	skaro.StartConnection(*host)
 }
